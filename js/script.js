8f404db6861a503336f563a04b6c9122c52f666d
artemkukol <arhi2319@gmail.com>
2024-08-28 01:56:44 +0300

    11

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// document.addEventListener('DOMContentLoaded', function () {
+// 	const burgerMenu = document.getElementById('burger-menu')
+// 	const navLinks = document.getElementById('nav-links')
+
+// 	burgerMenu.addEventListener('click', function () {
+// 		navLinks.classList.toggle('nav-active')
+// 	})
+// })
+
+// burgerMenu.addEventListener('click', function () {
+// 	navLinks.classList.toggle('nav-active')
+// 	burgerMenu.classList.toggle('active')
+// })
